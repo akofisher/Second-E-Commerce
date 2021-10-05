a16e6bc67beae75f6cb1897fc2ba5c6522c6f405
akofisher <akolekveishvili@gmail.com>
2021-10-05 20:36:08 +0400

    managed states and overview component

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,24 +1,10 @@
 import React from 'react'
-import CollectionPreview from '../../components/preview-collection/preview-collection.component'
-import SHOP_DATA from './shop.data'
+import CollectionOverview from '../../components/collections-overview/collections-overview.component'
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      collections: SHOP_DATA,
-    }
-  }
-  render() {
-    const { collections } = this.state
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    )
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    <CollectionOverview />
+  </div>
+)
 
 export default ShopPage
